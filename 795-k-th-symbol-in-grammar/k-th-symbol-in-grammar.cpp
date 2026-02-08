1aa62138e57cc305c838fdeb2e9f315b4b904f2e
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-02-08 23:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (11.33%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1 && k==1) return 0;
+        int temp=pow(2,n-1)/2;
+        if(k<=temp) return kthGrammar(n-1,k);
+        else return !kthGrammar(n-1,k-temp);
+    }
+};
