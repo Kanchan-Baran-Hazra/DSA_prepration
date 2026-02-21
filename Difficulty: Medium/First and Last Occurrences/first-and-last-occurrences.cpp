76e7bb7ce82ff5c1a5710af5211dbfd3bbdf0057
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-02-21 23:30:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
@@ -1,25 +1,68 @@
 class Solution {
   public:
+    int findLower(vector<int>& arr, int j, int x){
+        int i=0;
+        int ans=j;
+        
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            
+            if(arr[mid]==x){
+                ans=mid;
+                j=mid-1;
+            }else i=mid+1;
+        }
+        
+        return ans;
+    }
+    
+    int finfUpper(vector<int>& arr, int i, int x){
+        int j=arr.size()-1;
+        int ans=arr.size();
+        
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            
+            if(arr[mid]>x){
+                ans=mid;
+                j=mid-1;
+            }else i=mid+1;
+        }
+        
+        return ans;
+    }
+    
     vector<int> find(vector<int>& arr, int x) {
         // code here
         vector<int>ans;
-        ans.push_back(-1);
-        ans.push_back(-1);
+        if(arr[arr.size()-1]<x){
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+        if(arr[0]>x){
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
         int i=0;
         int j=arr.size()-1;
+        
         while(i<=j){
             int mid=i+(j-i)/2;
+            
             if(arr[mid]==x){
-                int n=mid;
-                int m=mid;
-                while(n>0 && arr[n-1]==x) n--;
-                while(m<arr.size()-1 && arr[m+1]==x) m++;
-                ans[0]=n;
-                ans[1]=m;
+                int low=findLower(arr,mid,x);
+                int upper=finfUpper(arr,mid+1,x);
+                ans.push_back(low);
+                ans.push_back(upper-1);
                 return ans;
             }else if(arr[mid]<x) i=mid+1;
             else j=mid-1;
         }
+        
+        ans.push_back(-1);
+        ans.push_back(-1);
         return ans;
     }
 };
