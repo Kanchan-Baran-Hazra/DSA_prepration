97896e6b0f48d16761cb0c959131462167861b74
kanchan-b-hazra <kbhazra800@gmail.com>
2026-02-22 17:45:07 +0530

    tree from preorder data

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<"Hello World!"<<endl;
+    // cout<<isalnum('A')<<endl;
+    cout<<stoi("1")+stoi("4")<<endl;
+    return 0;
+}
