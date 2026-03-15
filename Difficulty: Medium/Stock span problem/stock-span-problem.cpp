4a4c687c417eb8d841a91d1cb3dfd69be8314062
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-03-15 11:07:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -2,19 +2,20 @@ class Solution {
   public:
     vector<int> calculateSpan(vector<int>& arr) {
         // code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        stack<int>pq;
         vector<int>ans;
         for(int i=0;i<arr.size();i++){
-            while(pq.size()!=0 && pq.top().first<=arr[i]){
+            while(pq.size()!=0 && arr[pq.top()]<=arr[i]){
                 pq.pop();
             }
             if(pq.size()==0){
                 ans.push_back(i+1);
             }else{
-                ans.push_back(i-pq.top().second);
+                ans.push_back(i-pq.top());
             }
-            pq.push({arr[i],i});
+            pq.push(i);
         }
+        
         return ans;
     }
 };
