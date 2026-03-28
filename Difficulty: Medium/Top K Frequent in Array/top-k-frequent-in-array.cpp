64db2579355e590bee232946e72846309795448f
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-03-29 00:02:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
+++ Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
@@ -1,26 +1,28 @@
 class Solution {
   public:
-    static bool cmp(pair<int,int>a,pair<int,int>b){
-        if(a.second!=b.second){
-            return a.second>b.second;
-        }
-        return a.first>b.first;
+    static bool emp(pair<int,int>a,pair<int,int>b){
+        if(a.second==b.second) return a.first>b.first;
+        return a.second>b.second;
     }
     vector<int> topKFreq(vector<int> &arr, int k) {
         // Code here
+        vector<int>ans;
         unordered_map<int,int>mp;
+        vector<pair<int,int>>temp;
         for(int i=0;i<arr.size();i++){
             mp[arr[i]]++;
         }
-        vector<pair<int,int>>temp;
+        
         for(auto x:mp){
             temp.push_back(x);
         }
-        sort(temp.begin(),temp.end(),cmp);
-        vector<int>ans;
+        
+        sort(temp.begin(),temp.end(),emp);
+        
         for(int i=0;i<k;i++){
             ans.push_back(temp[i].first);
         }
+        
         return ans;
     }
 };
