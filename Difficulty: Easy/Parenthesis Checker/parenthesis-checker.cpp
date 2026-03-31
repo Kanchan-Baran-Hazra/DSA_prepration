b8a15eddac15a06d6de503cba8f9f5751efda79b
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-03-31 20:11:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -4,17 +4,21 @@ class Solution {
         // code here
         stack<char>st;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            if(st.size()==0 || s[i]=='(' || s[i]=='{' || s[i]=='['){
                 st.push(s[i]);
-            }
-            else{
-                if(st.size()==0) return false;
-                if(s[i]==')' && st.top()!='(') return false;
-                if(s[i]=='}' && st.top()!='{') return false;
-                if(s[i]==']' && st.top()!='[') return false;
-                st.pop();
+            }else{
+                if(s[i]==')' && st.top()=='('){
+                    st.pop();
+                }else if(s[i]=='}' && st.top()=='{'){
+                    st.pop();
+                }else if(s[i]==']' && st.top()=='['){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
         }
-        return st.size()==0? true:false;
+        
+        return st.size()==0;
     }
 };
