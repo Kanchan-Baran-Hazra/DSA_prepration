9a56da298677fa52a6e0d4dc101a2e484c4dd67c
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-03-31 19:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (24.39%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+
+        for(int i=0;i<s.size();i++){
+            if(st.empty() || 
+            !((st.top() == 'A' && s[i] == 'B') || 
+            (st.top() == 'C' && s[i] == 'D')))
+            {
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+            }
+        }
+        return st.size();
+    }
+};
