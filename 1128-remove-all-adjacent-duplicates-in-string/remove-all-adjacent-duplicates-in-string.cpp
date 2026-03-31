779760740ad725eb4e171151c598cb87c80c4f63
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-03-31 18:11:09 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (20.58%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(st.size()==0 || st.top()!=s[i]){
+                st.push(s[i]);
+            }
+            else{
+                // while(st.size()!=0 && st.top()==s[i]){
+                //     st.pop();
+                // }
+                st.pop();
+            }
+        }
+
+        string ans="";
+        while(st.size()!=0){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
