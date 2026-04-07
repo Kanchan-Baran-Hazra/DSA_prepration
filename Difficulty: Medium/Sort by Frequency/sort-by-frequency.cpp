be9c438f7e1367981a0ec8de15d10edc177ac963
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-04-07 18:18:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort by Frequency/sort-by-frequency.cpp
+++ Difficulty: Medium/Sort by Frequency/sort-by-frequency.cpp
@@ -1,29 +1,34 @@
 class Solution {
-public:
-    static bool emp(pair<char,int>a, pair<char,int>b){
-        if(a.second == b.second) return a.first < b.first;
-        return a.second < b.second;
-    }
-
-    string frequencySort(string &s) {
-        unordered_map<char,int> mp;
-        vector<pair<char,int>> temp;
-        string ans = "";
-
-        for(char c : s){
-            mp[c]++;
+  public:
+    static bool emp(pair<char,int> &a,pair<char,int> &b){
+        if(a.second==b.second){
+            return a.first<b.first;
         }
-
-        for(auto x : mp){
+        
+        return a.second<b.second;
+    }
+    string frequencySort(string &s) {
+        // code here
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        
+        vector<pair<char,int>>temp;
+        for(auto x:mp){
             temp.push_back(x);
         }
-
-        sort(temp.begin(), temp.end(), emp);
-
-        for(auto &p : temp){
-            ans.append(p.second, p.first); // 🔥 best way
+        
+        string ans;
+        
+        sort(temp.begin(),temp.end(),emp);
+        
+        for(int i=0;i<temp.size();i++){
+            for(int j=0;j<temp[i].second;j++){
+                ans.push_back(temp[i].first);
+            }
         }
-
+        
         return ans;
     }
 };
