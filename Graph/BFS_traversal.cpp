dce261f147c0066db181ccf5387fbd7f4ddc7c1d
Kanchan-Baran-Hazra <kanchanbhazra7063@gmail.com>
2026-06-04 10:38:58 +0530

    bfs in graph

---
+++ Graph/BFS_traversal.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Graph{
+public:
+    int V;
+    vector<vector<int>> adjList;
+
+    Graph(int V){
+        this->V = V;
+        adjList.resize(V);
+    }
+
+    void addEdge(int u, int v){
+        adjList[u].push_back(v);
+        adjList[v].push_back(u); // For undirected graph
+    }
+
+    void printGraph(){
+        for(int i = 0; i < V; i++){
+            cout << i << ": ";
+            for(int j = 0; j < adjList[i].size(); j++){
+                cout << adjList[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void BFs(){
+        queue<int>Q;
+        vector<bool>visited(V,false);
+
+        Q.push(0);
+        visited[0]=true;
+
+        while(Q.size()!=0){
+            int x=Q.front();
+            Q.pop();
+
+            cout<<x<<endl;
+            for(int i=0;i<adjList[x].size();i++){
+                if(!visited[adjList[x][i]]){
+                    Q.push(adjList[x][i]);
+                    visited[adjList[x][i]]=true;
+                }
+            }
+        }
+    }
+};
+
+int main(){
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(3,2);
+    g.addEdge(2,4);
+
+    // g.printGraph();
+    g.BFs();
+
+    return 0;
+}
