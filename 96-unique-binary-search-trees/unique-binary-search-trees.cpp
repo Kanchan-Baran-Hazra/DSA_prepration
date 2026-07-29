528ea50df255fafe77a865da62923e41173ecd19
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-07-29 21:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (18.66%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static int helper(int n, vector<int>& dp) {
+        if (n <= 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        dp[n] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[n] += helper(i - 1, dp) * helper(n - i, dp);
+        }
+
+        return dp[n];
+    }
+
+    int numTrees(int n) {
+        vector<int> dp(n + 1, -1);
+        dp[0] = dp[1] = 1;
+        return helper(n, dp);
+    }
+};
