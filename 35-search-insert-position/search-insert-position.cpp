7ae26fd77f414e783dd9c066d63ca7f093583d1a
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-08-15 19:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (78.63%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 class Solution {
 public:
+    static int get_lower_bound(vector<int>& nums,int target){
+        int i=0;
+        int j=nums.size()-1;
+
+        while(i<=j){
+            int mid=i+(j-i)/2;
+
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) j=mid-1;
+            else i=mid+1;
+        }
+
+        return i;
+    }
     int searchInsert(vector<int>& nums, int target) {
-        auto x= lower_bound(nums.begin(),nums.end(),target);
-        return x-nums.begin();
+        int x= get_lower_bound(nums,target);
+        return x;
     }
 };
