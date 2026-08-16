3ba33f86e67b1c45c4226d7a73f03ee099942285
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-08-16 13:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (89.81%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        auto x=upper_bound(letters.begin(),letters.end(),target);
+        if(x==letters.end()) return letters[0];
+        return *x;
+    }
+};
