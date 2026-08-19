08175cc42c5044df586fd354ae6706bafc83c2e7
Kanchan Baran Hazra <kanchanbhazra7063@gmail.com>
2026-08-19 09:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (16.63%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int currentPos=nums.size()-1;
-        for(int i=nums.size()-2;i>=0;i--){
-            if(i+nums[i]>=currentPos){
-                currentPos=i;
-            }
+        int n=nums.size();
+        if(n==1) return true;
+        int need=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]+i>=need) need=i;
         }
-
-        return currentPos==0;
+        if(need>0) return false;
+        return true;
     }
 };
